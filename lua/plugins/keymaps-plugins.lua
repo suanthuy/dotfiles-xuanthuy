3667344766162035182d19e73cd7fb4f930dc06a
oceanus <suanthuy12@gmail.com>
2024-09-26 21:59:22 +0700

    Update keymaps-plugins with telescope and nvim-tree keymaps

--- lua/plugins/keymaps-plugins.lua
+++ lua/plugins/keymaps-plugins.lua
@@ -1,9 +1,19 @@
 
-
-
 -- icon-picker.nvim
 local opts = { noremap = true, silent = true }
-
 vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
 vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
 vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- nvim-tree
+vim.keymap.set('n', '<leader>to', '<cmd>:NvimTreeOpen<CR>', { desc = 'Nvim-tree open and focus'})
+vim.keymap.set('n', '<leader>tc', '<cmd>:NvimTreeClose<CR>', { desc = 'Nvim-tree close'})
+vim.keymap.set('n', '<leader>tt', '<cmd>:NvimTreeToggle<CR>', { desc = 'Nvim-tree open or close'})
+
