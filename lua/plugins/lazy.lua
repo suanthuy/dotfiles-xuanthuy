106ed93592c0e30520be1f5e9652f5c1a66b3a95
oceanus <suanthuy12@gmail.com>
2024-09-27 23:58:54 +0700

    Add nvim-cmp, nvim-cursorline, nvim-lspconfig, trouble

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -78,7 +78,15 @@ require("lazy").setup({
     { "onsails/lspkind.nvim" },
 
     -- fast git decorations implemented
-    { "lewis6991/gitsigns.nvim" },
+    { "lewis6991/gitsigns.nvim",
+        signs = {
+            add = { text = '+' },
+            change = { text = '~' },
+            delete = { text = '_' },
+            topdelete = { text = '‾' },
+            changedelete = { text = '~' },
+        },
+    },
 
     -- a Neovim plugin that helps you pick font characters, symbol, nerd font icon
     {
@@ -247,8 +255,66 @@ require("lazy").setup({
             { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
             { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
         },
-    }
+    },
 
+    -- a pretty list for showing diagnostics, references, telescope result, quickfix and location lists to help you solve all the trouble your code is causing
+    {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+    },
+
+    -- LSP Configuration & Plugins
+    -- language server
+    { 
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            -- Automatically install LSPs to stdpath for neovim
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+
+            -- Useful status updates for LSP
+            'j-hui/fidget.nvim',
+        }
+    },
+
+
+    -- Autocompletion
+    { 
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        dependencies = {
+            -- Snippet Engine & its associated nvim-cmp source
+            {
+                'L3MON4D3/LuaSnip',
+                build = (function()
+                    -- Build Step is needed for regex support in snippets
+                    -- This step is not supported in many windows environments
+                    -- Remove the below condition to re-enable on windows
+                    if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+                        return
+                    end
+                    return 'make install_jsregexp'
+                end)(),
+            },
+            'saadparwaiz1/cmp_luasnip',
+
+            -- Adds other completion capabilities.
+            --  nvim-cmp does not ship with all sources by default. They are split
+            --  into multiple repos for maintenance purposes.
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
+
+            -- If you want to add a bunch of pre-configured snippets,
+            --    you can use this plugin to help you. It even has snippets
+            --    for various frameworks/libraries/etc. but you will have to
+            --    set up the ones that are useful for you.
+            -- 'rafamadriz/friendly-snippets',
+        },
+    },
+
+    -- Highlight words and lines
+    { "yamatsum/nvim-cursorline" },
 
 })
 
